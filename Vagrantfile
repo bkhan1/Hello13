ef12d05590dff475f911ab2281f1a78ae2cb1dee
beenishk <beenish.khan1@gmail.com>
2015-08-31 00:58:11 -0400

    All the vagrant + Chef provisioning work.

--- Vagrantfile
+++ Vagrantfile
@@ -8,5 +8,27 @@
 # workstation should display the rendered index page.
 
 Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "ubuntu/trusty32"
+
+#  config.vm.provider :virtualbox do |vb|
+#	vb.gui = true
+#  end
+
+  config.vm.synced_folder ".", "/srv/hello13", create: true, group: "vagrant", owner: "vagrant"
+  
+  config.vm.network :forwarded_port, guest: 80, host: 8181
+ 
+  config.berkshelf.berksfile_path = 'chef/cookbooks/hello13/Berksfile'
+  config.berkshelf.enabled = true
+  
+  config.vm.provision "chef_solo" do |chef|
+	chef.cookbooks_path = "chef/cookbooks/hello13"
+	chef.add_recipe :apt
+	chef.add_recipe 'nginx'
+	chef.add_recipe 'php-fpm'
+	chef.add_recipe 'hello13'
+  end 
+  
 end
+
+
--- chef/cookbooks/hello13/recipes/default.rb
+++ chef/cookbooks/hello13/recipes/default.rb
@@ -1,7 +1,32 @@
-# 
-# TODO:
-#   1) Install Nginx
-#   2) Install PHP
-#   3) Configure Nginx to listen on port 8181, serving PHP content from
-#      /srv/hello13/src at /, and static content from /srv/hello13/static
-#      at /static.
+service 'nginx' do
+	action [:enable, :start]
+end	
+
+template "/etc/nginx/nginx.conf" do
+  owner "vagrant"
+  group "vagrant"
+  mode "0644"
+  source "nginx.conf.erb"
+  notifies :restart, 'service[nginx]', :delayed
+end
+
+nginx_site 'default' do
+  enable false
+end
+
+link '/etc/nginx/sites-enabled/hello13' do
+  to '/etc/nginx/sites-available/hello13'
+end
+
+
+file "/etc/nginx/sites-available/default" do
+  action :delete
+  notifies :restart, 'service[nginx]', :delayed
+end
+
+service "php5-fpm" do
+  action :start
+end  
+
+
+
